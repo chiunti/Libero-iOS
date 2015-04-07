9b016f2b9c65bda2f1f804b0930ef18535445d67
chiunti <chiunti_abc@hotmail.com>
2015-04-07 00:58:39 -0500

    fix quota

--- libero/PlayerFeeTVC.m
+++ libero/PlayerFeeTVC.m
@@ -11,19 +11,99 @@
 #import "CellPlayerQuota.h"
 #import "Globals.h"
 
+UIView *vQuota;
+UIView *backgroundView;
+CGFloat altura = 200;
+PFObject *currentAbono;
+UISwitch *sw;
+UITextField *txtView;
+UILabel *lblCuota;
+UILabel *lblDebe;
+
 @interface PlayerFeeTVC ()
 
 @end
 
+
 @implementation PlayerFeeTVC
 
 
+
 - (void)viewDidLoad {
+
+    
     [super viewDidLoad];
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(loadObjects) name:@"NewQuota" object:nil];
     
+    [self createCaptureView];
+
+    
 }
+
+-(void) createCaptureView
+{
+    backgroundView = [[UIView alloc] initWithFrame:CGRectMake(0, 0,
+                                                              [UIScreen mainScreen].bounds.size.width,
+                                                              [UIScreen mainScreen].bounds.size.height)
+                      ];
+    backgroundView.backgroundColor = [[UIColor grayColor] colorWithAlphaComponent:.5f];
+    
+    
+    vQuota = [[UIView alloc] initWithFrame:CGRectMake(0, -altura, [UIScreen mainScreen].bounds.size.width, altura) ];
+    [vQuota setBackgroundColor: [UIColor whiteColor]];
+    vQuota.layer.borderColor    = [UIColor clearColor].CGColor;
+    vQuota.layer.borderWidth    = 1;
+    vQuota.clipsToBounds        = YES;
+    vQuota.layer.cornerRadius   = 10;
+    
+    // Create a label and add it to the view.
+    CGRect labelFrame = CGRectMake( 0, 0, [UIScreen mainScreen].bounds.size.width, 30 );
+    UILabel* label = [[UILabel alloc] initWithFrame: labelFrame];
+    [label setTextAlignment:NSTextAlignmentCenter];
+    [label setText: @"Líbero: Cuota"];
+    [label setBackgroundColor:[UIColor blueColor]];
+    [label setTextColor:[UIColor whiteColor]];
+    label.layer.cornerRadius = 8;
+    [vQuota addSubview: label];
+    
+    
+    // Create a label and add it to the view.
+    labelFrame = CGRectMake( 10, 30, [UIScreen mainScreen].bounds.size.width-20, 30 );
+    lblCuota = [[UILabel alloc] initWithFrame: labelFrame];
+    [vQuota addSubview: lblCuota];
+    
+    
+    labelFrame = CGRectMake( 10, 60, [UIScreen mainScreen].bounds.size.width-20, 30 );
+    txtView = [[UITextField alloc] initWithFrame:labelFrame];
+    [txtView setPlaceholder:@"Abono"];
+    txtView.keyboardType = UIKeyboardTypeDecimalPad;
+    txtView.borderStyle = UITextBorderStyleRoundedRect;
+    txtView.textAlignment = NSTextAlignmentRight;
+    [vQuota addSubview: txtView];
+    
+    labelFrame =  CGRectMake( 10, 130, 130, 30 );
+    UIButton *btnCancel = [[UIButton alloc] initWithFrame:labelFrame];
+    [btnCancel setTitle:@"Cancelar" forState:UIControlStateNormal];
+    btnCancel.layer.cornerRadius = 8;
+    [btnCancel addTarget:self
+               action:@selector(btnCancelPressed)
+     forControlEvents:UIControlEventTouchUpInside];
+    [btnCancel setBackgroundColor:[UIColor grayColor]];
+    [vQuota addSubview: btnCancel];
+    
+    
+    labelFrame =  CGRectMake( [UIScreen mainScreen].bounds.size.width-140, 130, 130, 30 );
+    UIButton *btnok = [[UIButton alloc] initWithFrame:labelFrame];
+    [btnok setTitle:@"Aceptar" forState:UIControlStateNormal];
+    btnok.layer.cornerRadius = 8;
+    [btnok addTarget:self
+                  action:@selector(btnOkPressed)
+        forControlEvents:UIControlEventTouchUpInside];
+    [btnok setBackgroundColor:[UIColor grayColor]];
+    [vQuota addSubview: btnok];
+}
+
 
 
 - (void) viewDidAppear:(BOOL)animated
@@ -187,7 +267,8 @@
 
 - (void) swPChanged:(id)sender
 {
-    UISwitch *sw = (UISwitch *) sender;
+    sw = (UISwitch *) sender;
+
     
     currentPlayer = self.objects[sw.tag];
     
@@ -199,11 +280,16 @@
     
     if (sw.isOn) {
         //nuevo registro
+        
+
+        
         PFObject *object = [PFObject objectWithClassName:@"cuotasjugador"];
         object[@"cuota"] = currentQuota;
         object[@"jugador"] = currentPlayer;
         
-        [object saveInBackground];
+        currentAbono = object;
+        [self verCaptura];
+        
     } else {
         // borrar registro
         for (PFObject *object in cuotas) {
@@ -212,5 +298,58 @@
         
     }
     
+}
+
+-(void)btnCancelPressed
+{
+    [sw setOn:false];
+    [self ocultarCaptura];
+}
+
+-(void)btnOkPressed
+{
+    //NSNumber *ndebe = [currentQuota objectForKey:@"importe"];
+    //ndebe =  [[NSNumber alloc] initWithDouble:[txtView.text doubleValue]];
+    
+    currentAbono[@"abono"] = [[NSNumber alloc] initWithDouble:[txtView.text doubleValue]];
+    //currentAbono[@"debe"] = ndebe;
+    [currentAbono saveInBackground];
+    [self ocultarCaptura];
+}
+
+-(void)verCaptura
+{
+    [txtView becomeFirstResponder];
+    [txtView setText:@""];
+    [lblCuota setText: [NSString stringWithFormat:@"%@: $%@", [currentQuota objectForKey:@"nombre"],[currentQuota objectForKey:@"importe"]]];
+    [self.parentViewController.view addSubview:backgroundView];
+    [self.parentViewController.view addSubview:vQuota];
+    
+    [UIView animateWithDuration:0.2
+                          delay:0
+                        options: UIViewAnimationOptionCurveEaseIn
+                     animations:^{
+                         vQuota.frame = CGRectMake(0, 50, [UIScreen mainScreen].bounds.size.width, altura);
+                     }
+                     completion:^(BOOL finished){
+                     }];
+  
+    
+}
+
+-(void)ocultarCaptura
+{
+    [UIView animateWithDuration:0.2
+                          delay:0
+                        options: UIViewAnimationOptionCurveEaseIn
+                     animations:^{
+                         vQuota.frame = CGRectMake(0, -altura, [UIScreen mainScreen].bounds.size.width, altura);
+                     }
+                     completion:^(BOOL finished){
+                         if (finished){
+                             [vQuota removeFromSuperview];
+                             [backgroundView removeFromSuperview];
+                         }
+                     }];
 }
 @end
